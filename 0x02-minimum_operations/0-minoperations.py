bd41dc8f05b5d8877126982f60904626d97b2e6d
AdonyasG <adonyas2000@gmail.com>
2023-04-03 10:18:00 -0400

    initial commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module - minioperation
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations needed
+    to result in exactly n H characters in the file
+    """
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 0
+    for i in range(2, n):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return n
