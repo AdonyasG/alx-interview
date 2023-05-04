a39c0f424b0c1eb1c883c9aa01a1c85928544fa7
AdonyasG <adonyas2000@gmail.com>
2023-05-04 18:23:31 -0400

    request changed to promise

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,19 +3,20 @@
 const request = require('request');
 const id = process.argv[2];
 
-request('https://swapi-api.alx-tools.com/api/films/' + id, function (error, response, body) {
+request('https://swapi-api.alx-tools.com/api/films/' + id, async function (error, response, body) {
   if (error) {
     console.error('Error:', error);
-  } else {
-    const data = JSON.parse(body).characters;
-    for (const character of data) {
-      request(character, function (error, response, body) {
+  }
+  const data = JSON.parse(body).characters;
+  for (const character of data) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
         if (error) {
           console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
         }
+        console.log(JSON.parse(body).name);
+        resolve();
       });
-    }
+    });
   }
 });
